9279f98aabd9a79a4e98891345f7e2184470f9c3
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-03 03:29:30 +0300

    FL11_HW6: Identify triangle type.

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,14 @@
 // Your code goes here
+let a = window.prompt('Please, enter triangle side length "a"', '');
+let b = window.prompt('Please, enter triangle side length "b"', '');
+let c = window.prompt('Please, enter triangle side length "c"', '');
+
+    if ( a + b <= c || a + c <= b || b + c <= a) {
+        console.log("Triangle doesn't exist")
+    } else if ( a + b > c || a + c > b|| b + c > a) {
+        console.log('Equivalent triangle')
+    } else if (a + b === b + c || b + c === c + a ) {
+        console.log('Isosceles triangle')
+    } else {
+        console.log('Normal triangle')
+    }
