d2132c6af2aa18e6f66a03cfb27991e6832c83e9
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-05 01:15:40 +0300

    FL11_HW7: Task 1: Check login and password done.

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,22 @@
-// Your code goes here
+let email = prompt('Hey, enter your "email" ', '');
+
+if (email === null || email === '') {
+    alert('"Canceled!"');
+
+} else if (email.length <= 6) {
+    alert(' “I don\'t know any emails having name length less than 6 symbols”. ');
+
+} else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
+    const password = prompt('Enter your password', '');
+
+    if (password === null || password === '') {
+        alert('"Canceled"');
+    } else if (password === 'UserPass' || password === 'AdminPass') {
+        alert('Logged in!')
+    } else {
+        alert('Wrong password');
+    }
+
+} else {
+    alert('"I don\'t know you! "');
+}
