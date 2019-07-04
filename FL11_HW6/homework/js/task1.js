02214e072ffa2c0a7608203c32d3a40ae74e981c
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-04 18:43:47 +0300

    FL11_HW6: Triangles.

---
+++ FL11_HW6/homework/js/task1.js
@@ -0,0 +1,14 @@
+let a1 = +window.prompt('Enter your value for point "A" on axis "x"', '5' );
+let a2 = +window.prompt('Enter your value for point "A" on axis "y"', '5');
+let b1 = +window.prompt('Enter your value for point "B" on axis "x"', '5');
+let b2 = +window.prompt('Enter your value for point "B" on axis "y"', '5');
+let c1 = +window.prompt('Enter your value for point "C" on axis "x"', '5');
+let c2 = +window.prompt('Enter your value for point "C" on axis "y"', '5');
+
+
+console.log(typeof a1);
+console.log(typeof a2);
+console.log(typeof b1);
+console.log(typeof b2);
+console.log(typeof c1);
+console.log(typeof c2);
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,14 +1,20 @@
-// Your code goes here
-let a = window.prompt('Please, enter triangle side length "a"', '');
-let b = window.prompt('Please, enter triangle side length "b"', '');
-let c = window.prompt('Please, enter triangle side length "c"', '');
+let a = Number(window.prompt('Please, enter triangle side length "a"', '0'));
+let b = Number(window.prompt('Please, enter triangle side length "b"', '0'));
+let c = Number(window.prompt('Please, enter triangle side length "c"', '0'));
 
     if ( a + b <= c || a + c <= b || b + c <= a) {
         console.log("Triangle doesn't exist")
     } else if ( a + b > c || a + c > b|| b + c > a) {
         console.log('Equivalent triangle')
-    } else if (a + b === b + c || b + c === c + a ) {
+    } else if ( a === b || b === c || a === c ) {
         console.log('Isosceles triangle')
     } else {
         console.log('Normal triangle')
     }
+
+    console.log(typeof a);
+    console.log(typeof b);
+    console.log(typeof c);
+    
+
+    
