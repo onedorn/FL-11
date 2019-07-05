602a7e9dac92080172f879de44d2406c534e8da1
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-06 00:59:53 +0300

    FL11_HW7: Step 2. P;aying the game.

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,19 +1,25 @@
 const game = confirm('Do you want to play a game?');
 
-
-// STEP 1: Start a game.
-if ( game === false) {
+  // STEP 1: Start a game.
+  if ( game === false) {
     alert('You did not become a billionaire, but can.');
   } 
-  
-  while (game === true) {
-    let num = 9;
-    const random = Math.floor(Math.random() * num );
-    const input = prompt(`
-      Enter the number between 0 and 8
-      Attempts left: 3
-      Total prize:  ${num}$
-      Possible prize on current attempt: ${num}$
+ 
+  // STEP 2. Playing the game.
+const answer = Math.floor(Math.random() * 9 );
+
+for (let i = 0; i < 3; i++) {
+  const guess = +prompt(
+    `Enter a number of pocket on which the ball could land 
+    Enter the number between 0 and 8
+    Attempts left: 3
+    Total prize:  ${0}$
+    Possible prize on current attempt: ${0}$
     `, '');
 
+    if (answer === guess ) {
+      alert('You guessed correctly!')
+    } else {
+      alert(`Thank you for your participation. Your prize is: ${0}`);
+    }
   }
