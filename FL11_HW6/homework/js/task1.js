e4a5f333e2f0afffcb28926446319b738894f771
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-05 16:59:22 +0300

    FL11_HW6: Ternary operator.

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,20 +1,14 @@
 const coordinates = {
-
     'a1': +prompt('Enter your value for point "A" on axis "x"', ''),
     'a2': +prompt('Enter your value for point "A" on axis "y"', ''),
     'b1': +prompt('Enter your value for point "B" on axis "x"', ''),
     'b2': +prompt('Enter your value for point "B" on axis "y"', ''),
     'c1': +prompt('Enter your value for point "C" on axis "x"', ''),
     'c2': +prompt('Enter your value for point "C" on axis "y"', '')
-
 }
 
 let divide = 2;
 let A = (coordinates.a1 + coordinates.b1) / divide;
 let B = (coordinates.a2 + coordinates.b2) / divide;
 
-    if(coordinates.c1 === A && coordinates.c2 === B) {
-        console.log(true)
-    } else {
-        console.log(false);
-    }
+console.log(coordinates.c1 === A && coordinates.c2 === B ? 'true' : 'false');
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,16 +1,28 @@
-let a = Number(window.prompt('Please, enter triangle side length "a"', '0'));
-let b = Number(window.prompt('Please, enter triangle side length "b"', '0'));
-let c = Number(window.prompt('Please, enter triangle side length "c"', '0'));
+// let a = Number(window.prompt('Please, enter triangle side length "a"', ''));
+// let b = Number(window.prompt('Please, enter triangle side length "b"', ''));
+// let c = Number(window.prompt('Please, enter triangle side length "c"', ''));
 
-    if ( a + b <= c || a + c <= b || b + c <= a) {
-        console.log("Triangle doesn't exist")
-    } else if ( a === b && b === c ) {
-        console.log('Equivalent triangle')
-    } else if ( a === b || b === c || a === c ) {
-        console.log('Isosceles triangle')
-    } else {
-        console.log('Normal triangle')
-    }
+    // if ( a + b <= c || a + c <= b || b + c <= a) {
+    //     console.log("Triangle doesn't exist")
+    // } else if ( a === b && b === c ) {
+    //     console.log('Equivalent triangle')
+    // } else if ( a === b || b === c || a === c ) {
+    //     console.log('Isosceles triangle')
+    // } else {
+    //     console.log('Normal triangle')
+    // }
     
 
+const sides = {
+  'a': +prompt('Please, enter triangle side length "a"', ''),
+  'b': +prompt('Please, enter triangle side length "b"', ''),
+  'c': +prompt('Please, enter triangle side length "c"', '')
+}
+
+ if (isNaN(sides.a) || isNaN(sides.b) || isNaN(sides.c) ) {
+    prompt('Its not a number, bro')
+  }
+
+
+  
     
