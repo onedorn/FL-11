08a33bd3d55aa24435778b4f1acf111e82b36b63
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-06 02:09:45 +0300

    FL11_HW7: Continue.

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -6,23 +6,31 @@ const game = confirm('Do you want to play a game?');
   } else {
 
   // STEP 2. Playing the game.
-const answer = Math.floor(Math.random() * 9 );
-const rounds = 3;
+  const assets = {
+    'rounds': 3,
+    'prize': '',
+    'total': '',
+    'random': 9
+  };
+  const answer = Math.floor(Math.random() * assets.random );
 
-for (let i = 0; i < rounds; i++) {
-  const guess = +prompt(
-    `Enter a number of pocket on which the ball could land 
-    Enter the number between 0 and 8
-    Attempts left: 3
-    Total prize:  ${0}$
-    Possible prize on current attempt: ${0}$
-    `, '');
-  
-      if (answer === guess ) {
+    for (let i = 0; i < assets.rounds; i++) {
+      
+      const guess = +prompt(
+        `
+        Enter a number of pocket on which the ball could land 
+        Enter the number between 0 and 8
+        Attempts left: ${assets.rounds}
+        Total prize:  ${assets.prize}$
+        Possible prize on current attempt: ${assets.total}$
+        `, '');
+      
+      if ( answer === guess ) {
         alert('You guessed correctly!')
         break;
+
       } else {
-        alert(`Ooops!!! You have ${rounds} attempts`);
+        confirm(`Thank you for your participation. Your prize is: ${assets.total}$`);
       }
     }
   }
