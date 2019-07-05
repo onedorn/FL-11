b86283a67ebbf4863b08d4d4fd49eaf3a3d47978
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-05 22:41:27 +0300

    FL11_HW7: Validated and well indented.

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -3,58 +3,58 @@ let password = '';
 let i = 5;
 let z = 6;
 
-    // Step 1. Check login
-    if ( email === '' || email === null ) {
+  // Step 1. Check login
+  if ( email === '' || email === null ) {
+    alert('Canceled!');
+
+  } else if ( email.length < z ) {
+    alert("I don't know any emails having name length less than 6 symbols.");
+
+  } else if ( email === 'user@gmail.com' || email === 'admin@gmail.com' ) {
+      password = prompt('Please, enter your password');
+      
+      // Step 2. Check password:
+      if ( password === '' || password === null ) {
         alert('Canceled!');
 
-    } else if ( email.length < z ) {
-        alert("I don't know any emails having name length less than 6 symbols.");
+      } else if ( password === 'UserPass' || password === 'AdminPass') {
 
-    } else if ( email === 'user@gmail.com' || email === 'admin@gmail.com' ) {
-       password = prompt('Please, enter your password');
-       
-        // Step 2. Check password:
-        if ( password === '' || password === null ) {
-            alert('Canceled!');
+      // Step 3. Change the password:    
+        let ask = confirm('Do you want to change your password?');
+          
+          if ( ask === false ) {
+            alert('You have failed the change.');
 
-        } else if ( password === 'UserPass' || password === 'AdminPass') {
+          } else {
+            let old = prompt('STEP 2: \nWrite your old password!');
+              
+              // Step 4. Old password check/ New password set
+              if( old === password ) {
+                let newPass = prompt('STEP 3: \nCreate new password!');
 
-        // Step 3. Change the password:    
-            let ask = confirm('Do you want to change your password?');
-            
-                if ( ask === false ) {
-                    alert('You have failed the change.');
+                  if( newPass.length < i ) {
+                      alert('It’s too short password. Sorry.”');
 
-                } else {
-                    let old = prompt('STEP 2: \nWrite your old password!');
-                    
-                    // Step 4. Old password check/ New password set
-                    if( old === password ) {
-                        let newPass = prompt('STEP 3: \nCreate new password!');
+                  } else if ( newPass.length > i ) {
+                      let newPassCheck = prompt('STEP 4: \nConfirm your password again!');
 
-                        if( newPass.length < i ) {
-                            alert('It’s too short password. Sorry.”');
+                      if(newPassCheck === newPass) {
+                        alert('You have successfully changed your password.')
 
-                        } else if ( newPass.length > i ) {
-                            let newPassCheck = prompt('STEP 4: \nConfirm your password again!');
+                      } else {
+                        alert('You wrote the wrong password.');
 
-                            if(newPassCheck === newPass) {
-                                alert('You have successfully changed your password.')
+                      }
 
-                            } else {
-                                alert('You wrote the wrong password.');
+                  } 
 
-                            }
+              } 
+          } 
 
-                        } 
+      } else {
+          alert( 'Wrong password' );
+      }
 
-                    } 
-                } 
-
-        } else {
-            alert( 'Wrong password' );
-        }
-
-    } else {
-        alert( "I don't know you!" );
-    }
+  } else {
+      alert( "I don't know you!" );
+  }
