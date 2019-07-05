86bb8ecf6412043bfc075578caae9d61ec3cd32a
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-05 23:37:41 +0300

    FL11_HW7: Announcment.

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,9 +1,19 @@
 const game = confirm('Do you want to play a game?');
 
-    if ( game === false) {
-        alert('You did not become a billionaire, but can.');
-    } else {
-        prompt('Enter a number of pocket on which the ball could land.', '').Math.floor(Math.random() * 9);
-    }
-    
-console.log(game);
+
+// STEP 1: Start a game.
+if ( game === false) {
+    alert('You did not become a billionaire, but can.');
+  } 
+  
+  while (game === true) {
+    let num = 9;
+    const random = Math.floor(Math.random() * num );
+    const input = prompt(`
+      Enter the number between 0 and 8
+      Attempts left: 3
+      Total prize:  ${num}$
+      Possible prize on current attempt: ${num}$
+    `, '');
+
+  }
