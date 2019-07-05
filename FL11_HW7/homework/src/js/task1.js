02bc99e94a373db0fc7ed05d580265bb9a350980
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-05 21:07:26 +0300

    FL11_HW7: Step 1. Check login.

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,39 +1,65 @@
-let email = prompt('Hey, enter your "email" ', '');
+// let email = prompt('Hey, enter your "email" ', '');
 
-if (email === null || email === '') {
-    alert('"Canceled!"');
+// if (email === null || email === '') {
+//     alert('"Canceled!"');
 
-} else if (email.length <= 6) {
-    alert(' “I don\'t know any emails having name length less than 6 symbols”. ');
+// } else if (email.length <= 6) {
+//     alert(' “I don\'t know any emails having name length less than 6 symbols”. ');
 
-} else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
-    const password = +prompt('Enter your password', '');
+// } else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
+//     const password = +prompt('Enter your password', '');
 
-    if (password === null || password === '') {
-        alert('"Canceled"');
-    } else if (password === 'UserPass' || password === 'AdminPass') {
-        let changePass = confirm('Do you want to change your password?', '');
+//     if (password === null || password === '') {
+//         alert('"Canceled"');
+//     } else if (password === 'UserPass' || password === 'AdminPass') {
+//         let changePass = confirm('Do you want to change your password?', '');
 
-        if (changePass === false) {
-            alert( '“You have failed the change.” ');
-        } else if (changePass === true ) {
-            let oldPass = +prompt('STEP 2: \nWrite you old password.', '');
+//         if (changePass === false) {
+//             alert( '“You have failed the change.” ');
+//         } else if (changePass === true ) {
+//             let oldPass = +prompt('STEP 2: \nWrite you old password.', '');
             
-            if (oldPass <=5 ) {
-                alert('“It’s too short password. Sorry.”');
-            } else if (oldPass > 5) {
-                let newPass = +prompt('STEP 3: \n Create new password!', '');
-            } else if (newPass === password) {
-                prompt('Enter new password again');
-            } 
+//             if (oldPass <=5 ) {
+//                 alert('“It’s too short password. Sorry.”');
+//             } else if (oldPass > 5) {
+//                 let newPass = +prompt('STEP 3: \n Create new password!', '');
+//             } else if (newPass === password) {
+//                 prompt('Enter new password again');
+//             } 
 
         
 
-        } else {
-            alert('Wrong password');
-        }
+//         } else {
+//             alert('Wrong password');
+//         }
 
-} else {
-alert('"I don\'t know you! "');
-}
-}
+// } else {
+// alert('"I don\'t know you! "');
+// }
+// }
+
+
+
+const email = prompt('Please, enter your "email" ', '');
+
+    // Step 1. Check login
+    if (email === '' || email === null) {
+        alert('Canceled!');
+
+    } else if (email.length < 6 ) {
+        alert("I don't know any emails having name length less than 6 symbols.");
+
+    } else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
+        +prompt('Please, enter your password');
+
+    } else {
+        alert("I don't know you!");
+    }
+
+
+const password = '';
+
+     // Step 2. Check password:
+    if (password === '' || password === null) {
+        alert('Canceled!');
+    }
--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,9 @@
-// Your code goes here
+const game = confirm('Do you want to play a game?');
+
+    if ( game === false) {
+        alert('You did not become a billionaire, but can.');
+    } else {
+        prompt('Enter a number of pocket on which the ball could land.', '').Math.floor(Math.random() * 9);
+    }
+    
+console.log(game);
