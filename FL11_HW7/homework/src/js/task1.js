d28f2ce808b6212795cf1ef396cf186228d241a1
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-05 22:27:51 +0300

    FL11_HW7: Step 3/4. Change and check new pass.

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,69 +1,60 @@
-// let email = prompt('Hey, enter your "email" ', '');
-
-// if (email === null || email === '') {
-//     alert('"Canceled!"');
-
-// } else if (email.length <= 6) {
-//     alert(' “I don\'t know any emails having name length less than 6 symbols”. ');
-
-// } else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
-//     const password = +prompt('Enter your password', '');
-
-//     if (password === null || password === '') {
-//         alert('"Canceled"');
-//     } else if (password === 'UserPass' || password === 'AdminPass') {
-//         let changePass = confirm('Do you want to change your password?', '');
-
-//         if (changePass === false) {
-//             alert( '“You have failed the change.” ');
-//         } else if (changePass === true ) {
-//             let oldPass = +prompt('STEP 2: \nWrite you old password.', '');
-            
-//             if (oldPass <=5 ) {
-//                 alert('“It’s too short password. Sorry.”');
-//             } else if (oldPass > 5) {
-//                 let newPass = +prompt('STEP 3: \n Create new password!', '');
-//             } else if (newPass === password) {
-//                 prompt('Enter new password again');
-//             } 
-
-        
-
-//         } else {
-//             alert('Wrong password');
-//         }
-
-// } else {
-// alert('"I don\'t know you! "');
-// }
-// }
-
-
-
 const email = prompt('Please, enter your "email" ', '');
 let password = '';
+let i = 5;
+let z = 6;
 
     // Step 1. Check login
-    if (email === '' || email === null) {
+    if ( email === '' || email === null ) {
         alert('Canceled!');
 
-    } else if (email.length < 6 ) {
+    } else if ( email.length < z ) {
         alert("I don't know any emails having name length less than 6 symbols.");
 
-    } else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
-       console.log( password = prompt('Please, enter your password'));
+    } else if ( email === 'user@gmail.com' || email === 'admin@gmail.com' ) {
+       password = prompt('Please, enter your password');
        
         // Step 2. Check password:
-        if (password === '' || password === null) {
+        if ( password === '' || password === null ) {
             alert('Canceled!');
 
         } else if ( password === 'UserPass' || password === 'AdminPass') {
-            confirm('Do you want to change your password?');
+
+        // Step 3. Change the password:    
+            let ask = confirm('Do you want to change your password?');
+            
+                if ( ask === false ) {
+                    alert('You have failed the change.');
+
+                } else {
+                    let old = prompt('STEP 2: \nWrite your old password!');
+                    
+                    // Step 4. Old password check/ New password set
+                    if( old === password ) {
+                        let newPass = prompt('STEP 3: \nCreate new password!');
+
+                        if( newPass.length < i ) {
+                            alert('It’s too short password. Sorry.”');
+
+                        } else if ( newPass.length > i ) {
+                            let newPassCheck = prompt('STEP 4: \nConfirm your password again!');
+
+                            if(newPassCheck === newPass) {
+                                alert('You have successfully changed your password.')
+
+                            } else {
+                                alert('You wrote the wrong password.');
+
+                            }
+
+                        } 
+
+                    } 
+                } 
 
         } else {
-            alert('Wrong password');
+            alert( 'Wrong password' );
         }
 
     } else {
-        alert("I don't know you!");
+        alert( "I don't know you!" );
     }
