c99a083a64a340a5ef36dafaf4c96c2d795d9855
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-06 01:23:44 +0300

    FL11_HW7: STEP 2. Looping a cycle.

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -3,12 +3,13 @@ const game = confirm('Do you want to play a game?');
   // STEP 1: Start a game.
   if ( game === false) {
     alert('You did not become a billionaire, but can.');
-  } 
- 
+  } else {
+
   // STEP 2. Playing the game.
 const answer = Math.floor(Math.random() * 9 );
+const rounds = 3;
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < rounds; i++) {
   const guess = +prompt(
     `Enter a number of pocket on which the ball could land 
     Enter the number between 0 and 8
@@ -16,10 +17,13 @@ for (let i = 0; i < 3; i++) {
     Total prize:  ${0}$
     Possible prize on current attempt: ${0}$
     `, '');
-
-    if (answer === guess ) {
-      alert('You guessed correctly!')
-    } else {
-      alert(`Thank you for your participation. Your prize is: ${0}`);
+  
+      if (answer === guess ) {
+        alert('You guessed correctly!')
+        break;
+      } else {
+        alert(`Ooops!!! You have ${rounds} attempts`);
+      }
     }
   }
+ 
