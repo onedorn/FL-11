b13f96133fd4eedee4472f7722cc8e552439c012
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-12 01:31:57 +0300

    FL11_HW8: Format time comments.

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -5,9 +5,8 @@ function formatTime (minutes) {
   
   let result = `${d} day{s} ${h} hours{s} ${m} minute(s)`;
       return result;
-      
 }
 
-formatTime(44567);
-formatTime(6654);
-formatTime(665);
+formatTime(44567); //30 day{s} 22 hours{s} 47 minute(s)
+formatTime(6654); //4 day{s} 14 hours{s} 54 minute(s)
+formatTime(665); //0 day{s} 11 hours{s} 5 minute(s)
