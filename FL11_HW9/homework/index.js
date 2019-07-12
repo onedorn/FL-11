63f2694e3e12d94e3bf15cfa0e9e951799059cde
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-12 23:43:44 +0300

    FL11_HW9: All set.

---
+++ FL11_HW9/homework/index.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    '_id': '5b5e3168c6bf40f2c1235cd6',
+    'index': 0,
+    'birthday': '2016-03-18T00:00:00',
+    'eyeColor': 'green',
+    'name': 'Stein',
+    'favoriteFruit': 'apple'
+  },
+  {
+    '_id': '5b5e3168e328c0d72e4f27d8',
+    'index': 1,
+    'birthday': '1991-02-11T00:00:00',
+    'eyeColor': 'blue',
+    'name': 'Cortez',
+    'favoriteFruit': 'strawberry'
+  },
+  {
+    '_id': '5b5e3168cc79132b631c666a',
+    'index': 2,
+    'birthday': '1984-04-17T00:00:00',
+    'eyeColor': 'blue',
+    'name': 'Suzette',
+    'favoriteFruit': 'apple'
+  },
+  {
+    '_id': '5b5e31682093adcc6cd0dde5',
+    'index': 3,
+    'birthday': '1994-04-17T00:00:00',
+    'eyeColor': 'green',
+    'name': 'George',
+    'favoriteFruit': 'banana'
+  }
+]
+
+// Your code goes here
+
+
+
