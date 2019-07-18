3a508da39c918ade8c38bfaf026a457755e6ec07
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-19 01:54:04 +0300

    FL11_HW10: ESLint [ 0 errors, 0 wornings].

--- FL11_HW10/homework/js/fighters_game.js
+++ FL11_HW10/homework/js/fighters_game.js
@@ -1,12 +1,11 @@
-/* Task 1.*/
-const gameSettings = {loss: 0, wins: 0};
-
 const Fighter = class Fighter {
   constructor ({name, damage, health, agility}) {
     this.name = name;
     this.damage = damage;
     this.health = health;
     this.agility = agility;
+    this.wins = 0;
+    this.loses = 0;
   }
   getName() {
     return this.name;
@@ -20,7 +19,6 @@ const Fighter = class Fighter {
   getAgility() {
     return this.agility;
   }
-
   attack ( defender ) {
     let totalSuccessOfAttack = 100;
     let successOfAttack = totalSuccessOfAttack - defender.getAgility();
@@ -31,47 +29,44 @@ const Fighter = class Fighter {
     } else {
       console.log(`${this.name} attack missed`);
     }
-    return;
   }
-
   dealDamage( damage ) {
     this.health = this.health - damage;
     this.health < 0 ? this.health = 0 : this.health - damage;
   }
-
-  addWin() {
-    return gameSettings.wins++;
-  }
-  addLoss() {
-    return gameSettings.loss++;
-  }
-
   logCombatHistory() {
-    let message = `Name: ${this.name}, Wins:  ${this.addWin()}, Losses: ${this.addLoss()}`;
+    let message = `Name: ${this.name}, Wins: ${this.wins}, Losses: ${this.loses}`;
     return console.log(message);
   }
-
-  // heal() {}
-
+  heal( healPoints ) {
+    let totalHP = 100;
+    let currentHP = this.health;
+    let results = healPoints + currentHP;
+    results <= totalHP ? this.health += healPoints : console.log(`You can get only ${this.health = totalHP}`);
+  }
+  addWin() {
+    let score = this.wins += 1;
+    return score;
+  }
+  addLoss() {
+    let score = this.loses += 1;
+    return score;
+  }
 }
 
-const mario = new Fighter({name: 'Mario', damage: 20, health: 100, agility: 60});
-const luigi = new Fighter({name: 'Luigi', damage: 15, health: 100, agility: 90});
+const mario = new Fighter({name: 'Mario', damage: 25, health: 100, agility: 70});
+const luigi = new Fighter({name: 'Luigi', damage: 20, health: 100, agility: 50});
 
 const battle = ( warriorOne, warriorTwo ) => {
+
   while (warriorOne.health > 0 && warriorTwo.health > 0) {
 
-    if(warriorOne.health > 0 || warriorTwo.health > 0) {
-      warriorOne.attack( warriorTwo );
-    } 
-    if ( warriorOne.health > 0 || warriorTwo.health > 0 ) {
-      warriorTwo.attack( warriorOne );
-    } 
+    warriorOne.health > 0 || warriorTwo.health > 0 ? warriorOne.attack( warriorTwo ) : '';
+    warriorOne.health > 0 || warriorTwo.health > 0 ? warriorTwo.attack( warriorOne ) : '';
 
     warriorOne.health === 0 ? console.log(`${warriorOne.name} is dead and can't fight!`) : '';
     warriorTwo.health === 0 ? console.log(`${warriorTwo.name} is dead and can't fight!`) : '';
-    
   }
-  warriorOne.health < 0 ? warriorOne.addLoss() : warriorOne.addWin();
-  warriorTwo.health < 0 ? warriorTwo.addLoss() : warriorTwo.addWin();
+  warriorOne.health > 0 ? warriorOne.addWin() : warriorOne.addLoss();
+  warriorTwo.health > 0 ? warriorTwo.addWin() : warriorTwo.addLoss();
 }
