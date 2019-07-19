3da9ee783f93729a3969ccfddc9cfbb4d4784aa8
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-19 15:04:23 +0300

    FL11_HW11: Custom checkbox / Add & Remove todos.

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,3 +1,32 @@
 let rootNode = document.getElementById('root');
+const todosList = document.querySelector('.todos-list');
+const addForm = document.querySelector('.search');
 
-// Your code goes here
+// Add todos
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const todo = addForm.search.value.trim();
+  if(todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+const generateTemplate = todo => {
+  const html = `
+  <li>
+    <input type="checkbox" name="todo_list" id="checkbox-one">
+    <label for="checkbox-one">${todo}</label>
+    <i class="material-icons edit">edit</i>
+    <i class="material-icons delete">delete</i>
+  </li>
+  `;
+  todosList.innerHTML += html;
+};
+
+// Delete todos
+todosList.addEventListener('click', e=> {
+  if(e.target.classList.contains('delete')){
+    e.target.parentElement.remove();
+  }
+});
