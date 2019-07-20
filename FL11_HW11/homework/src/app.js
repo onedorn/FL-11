d0eafc4d7ef962b3d81d7f1db3eefb5ffaaf793a
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-07-20 23:09:46 +0300

    FL11_HW11: Notification.

--- FL11_HW11/homework/src/app.js
+++ FL11_HW11/homework/src/app.js
@@ -1,9 +1,11 @@
 let rootNode = document.getElementById('root');
 const todosList = document.querySelector('.todos-list');
 const addForm = document.querySelector('.search');
+const listItems = document.getElementsByTagName('li');
+const addTodo = document.querySelector('.add_box');
 
 // Add todos
-addForm.addEventListener('submit', e => {
+addTodo.addEventListener('click', e => {
   e.preventDefault();
   const todo = addForm.search.value.trim();
   if(todo.length) {
@@ -15,18 +17,23 @@ addForm.addEventListener('submit', e => {
 const generateTemplate = todo => {
   const html = `
   <li>
-    <input type="checkbox" name="todo_list" id="checkbox-one">
-    <label for="checkbox-one">${todo}</label>
+    <input type="checkbox" name="todo_list" id="checkbox">
+    <label for="checkbox">${todo}</label>
     <i class="material-icons edit">edit</i>
     <i class="material-icons delete">delete</i>
   </li>
   `;
   todosList.innerHTML += html;
+
+  // Create notification & disable button (if list full)
+  const notification = document.querySelector('.notification');
+  listItems.length > 9 ? notification.style.opacity = '1' : notification.style.opacity = '0';
 };
 
 // Delete todos
-todosList.addEventListener('click', e=> {
+todosList.addEventListener('click', e => {
   if(e.target.classList.contains('delete')){
     e.target.parentElement.remove();
   }
 });
+
