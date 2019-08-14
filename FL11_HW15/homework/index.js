2b0000c27bcaf74b48567c1dd93e09b279da006d
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-08-14 14:46:27 +0300

    FL11_HW15: Constructor with 2 props (1 private) & 2 methods.

--- FL11_HW15/homework/index.js
+++ FL11_HW15/homework/index.js
@@ -1,1 +1,19 @@
+// Create function constructor with props
+function Hamburger(type, calories) {
+
+  // Do calories field hidden from programmers.
+  let _calories = calories;
+  this.type = type
+
+  // Create two methods to work with props
+  this.getCalories = function() {
+    return _calories;
+  }
+  this.setCalories = function(calories) {
+    return _calories = calories;
+  }
+}
+
+const myHamburger = new Hamburger('classic', 600);
+console.log(myHamburger);
 
