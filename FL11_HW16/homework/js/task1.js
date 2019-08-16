f43b462bf6209a674caed5f68d2de623146c0114
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-08-16 21:25:59 +0300

    FL11_HW16: Assign method.

---
+++ FL11_HW16/homework/js/task1.js
@@ -0,0 +1,25 @@
+// 	Create your own assign method which mimics Object.assign.
+
+const course = {
+  name: 'Front-End JavaScript',
+  score: 57
+};
+
+const grade = {
+  score: 90
+};
+
+const assign = function(wrapper, ...merging_args) {
+  let main_wrapper = Object(wrapper);
+  for (let i = 0; i < merging_args.length; i++) {
+    const element = merging_args[i];
+    for (let key in element) {
+      if (Object.prototype.hasOwnProperty.call(element, key)) {
+        main_wrapper[key] = element[key];
+      }
+    }
+  }
+  return main_wrapper;
+}
+
+console.log(assign({}, course, grade, {teacher: 'Oleg Kustov'}));
