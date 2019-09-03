8fb4103bfecd04d2a2e04014254568c9ce52c3b8
Volodymyr Syvoraksha <Volodymyr_Syvoraksha@epam.com>
2019-09-03 16:12:35 +0300

    FL11_HW20: React JSX

--- FL_11_20_homework_react_jsx/app/src/App.js
+++ FL_11_20_homework_react_jsx/app/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Dashboard from "./components/Dashboard";
 import Shop from "./components/Shop";
-import "./App.css";
 
 class App extends Component {
   state = {
