ec976b910b103f346f434f77fd2ceb4a171441b6
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-09-25 15:31:02 +0300

    Add, remove, compleate todos.

--- FL_11_26_homewrork_jquery/task2/js/index.js
+++ FL_11_26_homewrork_jquery/task2/js/index.js
@@ -1,14 +1,44 @@
 const $list = $(".list");
 const $input = $("#add-input");
-const $add = $("#add-submit");
+const $add = "#add-submit";
+const $rm_btn = ".item-remove";
+const $completed_todo = '.item-text';
 
-const todos = [
-  {
-    text: "Buy milk",
-    done: false
-  },
-  {
-    text: "Play with dog",
-    done: true
+
+const actions = ((function () {
+  return {
+    paint_todo: function (e) {
+      e.preventDefault();
+      let text_content = $($input).val();
+      
+      if(text_content.length) {
+        let li = $('<li class="item"></li>');
+        let span = $(`<span class="item-text">${text_content}</span>`);
+        let button = $('<button class="item-remove">Remove</button>');
+        li.append(span, button);
+        $list.append(li);
+        $input.val('');
+      } 
+      
+      if(!text_content.length) {
+        alert('You can come in with something better');
+      }
+    },
+    delete_todo: function () {
+      let target_li = $(this).parent();
+      target_li.fadeOut(function () {
+        target_li.remove();
+      })
+    },
+    compleated_todo: function () {
+      $(this).addClass('done');
+    }
   }
-];
+})())
+
+
+$(document).ready(function () {
+  $(document).on('click', $add, actions.paint_todo);
+  $(document).on('click', $rm_btn, actions.delete_todo);
+  $(document).on('click', $completed_todo, actions.compleated_todo);
+})
