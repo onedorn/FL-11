9ebd1e9ed4947d0ee83f510c704c3264b8932731
Volodymyr Syvoraksha <vpsyvoraksha@gmail.com>
2019-09-25 16:08:07 +0300

    Local Storage

--- FL_11_26_homewrork_jquery/task2/js/index.js
+++ FL_11_26_homewrork_jquery/task2/js/index.js
@@ -6,36 +6,58 @@ const $completed_todo = '.item-text';
 
 
 const actions = ((function () {
+
+  const paint_todo = function (e) {
+    e.preventDefault();
+    let text_content = $($input).val();
+    storage.setTodoToLocalStorage(text_content);
+
+    if(text_content.length) {
+      let li = $('<li class="item"></li>');
+      let span = $(`<span class="item-text">${text_content}</span>`);
+      let button = $('<button class="item-remove">Remove</button>');
+      li.append(span, button);
+      $list.append(li);
+      $input.val('');
+    } 
+    if(!text_content.length) {
+      alert('You can come in with something better');
+    }
+  }
+
+  const delete_todo = function () {
+    let target_li = $(this).parent();
+    target_li.fadeOut(function () {
+      target_li.remove();
+    })
+  }
+
+  const compleated_todo = function () {
+    $(this).addClass('done');
+  }
+
   return {
-    paint_todo: function (e) {
-      e.preventDefault();
-      let text_content = $($input).val();
-      
-      if(text_content.length) {
-        let li = $('<li class="item"></li>');
-        let span = $(`<span class="item-text">${text_content}</span>`);
-        let button = $('<button class="item-remove">Remove</button>');
-        li.append(span, button);
-        $list.append(li);
-        $input.val('');
-      } 
-      
-      if(!text_content.length) {
-        alert('You can come in with something better');
-      }
+    paint_todo,
+    delete_todo,
+    compleated_todo
+  }
+
+})())
+
+const storage = ((function () {
+  return {
+    setTodoToLocalStorage: function (todo) {
+      let todos = [];
+      todos.push(todo);
+      localStorage.setItem('todos', JSON.stringify(todos));
     },
-    delete_todo: function () {
-      let target_li = $(this).parent();
-      target_li.fadeOut(function () {
-        target_li.remove();
-      })
-    },
-    compleated_todo: function () {
-      $(this).addClass('done');
+    getTodoFromLocalStorage: function () {
+      let todos;
+      todos = JSON.parse(localStorage.getItem('todos'));
+      return todos;
     }
   }
 })())
-
 
 $(document).ready(function () {
   $(document).on('click', $add, actions.paint_todo);
